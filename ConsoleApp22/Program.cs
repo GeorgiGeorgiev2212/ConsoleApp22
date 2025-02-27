07b90ab06f03705a0a017d35a7b8ffae6dedcdd2
Georgi Georgie <Georgi Georgie@DESKTOP-9FI4TKH>
2025-02-27 15:37:40 +0200

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+double suma = 100.0;
+
+int n = 12;
+Console.WriteLine($"Star: {suma:F2} lv");
+
+for (int month = 1; month <= n; month++ )
+{
+    
+    suma *= 1.3;
+    Console.WriteLine($"Month {month} : {suma} lv");
+    
+
+}
+double pechalba = (suma - 100.0) * 0.5;
+Console.WriteLine($"He will invest {pechalba:F} lv");
